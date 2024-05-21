ae8239342bb625523084d5719f631294e5084f8f
Boitumelo Thindiza <tthindiza@gmail.com>
2024-05-21 06:32:50 -0700

    Added links to my Github and portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+   <div className="App">
+    Weather App
+    <footer>
+      This project was coded by{""} <a href="https://www.linkedin.com/in/boitumelo-thindiza-222049159/" target="_blank"> Boitumelo Thindiza </a>{""} and is {""}
+      <a href="https://github.com/BThindiza/boitumelo-react-weather-app" target="_blank"> Open-Sourced on Github </a>
+    </footer>
+   </div> 
   );
 }
 
