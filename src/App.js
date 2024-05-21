9638f9e1d028ae81936056e5b542758de716f7f8
Boitumelo Thindiza <tthindiza@gmail.com>
2024-05-21 13:04:21 -0700

    added Bootstrap my html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App(){
   return(
    <div className="App">
+    <div className="container">
     Weather App
+   <Weather/>
     <footer>
       This project was coded by{""} <a href="https://www.linkedin.com/in/boitumelo-thindiza-222049159/" target="_blank"> Boitumelo Thindiza </a>{""} and is {""}
       <a href="https://github.com/BThindiza/boitumelo-react-weather-app" target="_blank"> Open-Sourced on Github </a>
     </footer>
    </div> 
+   </div>
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return(
+<div className="weather">
+
+<h1>Polokwane</h1>
+<ul>
+    <li>Tuesday 12:47</li>
+    <li>Sunny</li>
+</ul>
+<div className="row">
+    <div className="col-6">
+        
+    </div>
+</div>
+</div>
+);
+}
