48b6dea664785416867640674b4723918c3b621e
Boitumelo Thindiza <tthindiza@gmail.com>
2024-06-21 12:19:38 -0700

    Update weatherIcon.js

--- src/weatherIcon.js
+++ src/weatherIcon.js
@@ -21,10 +21,8 @@ export default function WeatherIcon(props){
     "13n":"SNOW", 
     "50d":"FOG",
     "50n":"FOG",
+};
 
-
-       
-    };
     return(
         <ReactAnimatedWeather
     icon={codeMapping[props.code]}
