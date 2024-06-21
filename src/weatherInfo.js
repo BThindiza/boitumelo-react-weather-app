1a1b836f1c62023043535febe9c11b6a7525b22b
Boitumelo Thindiza <tthindiza@gmail.com>
2024-06-21 11:20:46 -0700

    Made some changes

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -1,46 +1,45 @@
 import React from "react";
 import FormatDate from "./FormatDate";
 import WeatherIcon from "./WeatherIcon";
-import "./WeatherInfo.css";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
-    function formatWind(){
-        if (props.unit === "celsius"){
-            return `${Math.round(props.data.wind)} Km/h`;
-        } else{
-            return `${Math.round(props.data.wind * 0.62)} mph`;
-        }
-        }
-    }
     return(
         <div className="WeatherInfo">
-            <div className="row">
+            <h1>{props.data.city}</h1>
+            <ul>
+            <li><FormatDate date= {props.data.date}/></li> 
+            <li className="text-capitalize">{props.data.description}</li>
+            </ul>
+            
+            <div className="row mt-3">
                 <div className="col-6">
-<h1>{props.data.city}</h1>
-<ul>
-    <li><FormatDate date= {props.data.date}/>,
-{props.data.description}</li>
-<li className="humidity">Humidity:<strong>{props.data.humidity}%</strong> </li>
-<li className="wind"><strong>Wind:{formatWind()}</strong></li>
-</ul>
-</div>
-<div className="col-lg-6 col-md-6 col-sm-8">
-        <div className="temperature-container d-flex justify-content-end">
-         <WeatherIcon code={props.data.icon} size={52}/>   
-            <div>
-        <div>
-            <span className="temperature">
-                {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
-        </div>
-      <div>
-    </div>
+                    <div className="clearfix">
+                        <div className="float-left">
+                        <WeatherIcon code={props.data.icon} size={60}/> 
+                        </div>
 
-    </div>
-</div>
-</div>
-</div>
-</div>
+                        <div className="float-left">
+                            <WeatherTemperature celsius={props.data.temperature}/>
+                        </div>
+                        </div>
+                        </div>
+
+                        <div className="float-left">
+                            <div className="float-right">
+                                <ul>
+                                    <li> Humidity: {props.data.humidity}%</li>
+                                    <li>Wind: {props.data.wind}Km/h</li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                        </div>
     );
 }
+
+
+
+                   
+
+
